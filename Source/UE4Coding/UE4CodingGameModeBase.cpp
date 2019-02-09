a433a1f4650dcc1d00d894cf62dfee2fd92fcf03
Anastasia Ionicioiu <anastasia@lifecycle.today>
2019-02-09 23:28:17 +0200

    getting closer to finish PZ1. But the game crushes. The probable error: ConstructorHelpers can't be used outside of constructor!

--- Source/UE4Coding/UE4CodingGameModeBase.cpp
+++ Source/UE4Coding/UE4CodingGameModeBase.cpp
@@ -1,21 +1,51 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "UE4CodingGameModeBase.h"
+#include "ConstructorHelpers.h"
 
 void AUE4CodingGameModeBase::StartPlay()
 {
 	Super::StartPlay();
 
-	UFloatProperty* FloatProp = FindField<UFloatProperty>(this->GetClass(), TEXT("BpFloat"));
-	if (FloatProp != nullptr)
+	// Showing if this GameMode is active
+	UE_LOG(LogTemp, Warning, TEXT("Starting EU4Coding GameMode"));
+
+	// *** ог 1 ***
+	// 
+	// accessing and changing a blueprint variable
+
+
+	// Finding RandomNumber blueprint
+	// My Blueprint reference: Blueprint'/Game/PZ1/BP_RandomNumber.BP_RandomNumber'
+	ConstructorHelpers::FObjectFinder<UBlueprint> BlueprintObj(TEXT("Blueprint'/Game/PZ1/BP_RandomNumber.BP_RandomNumber'"));
+
+	UBlueprint* blueprint = NULL;
+	if (BlueprintObj.Succeeded()) {
+		blueprint = BlueprintObj.Object;
+		// if the blueprint is fount, congratulate
+		UE_LOG(LogTemp, Warning, TEXT("The RandomNumber blueprint was found!"));
+	} else {
+		// if the blueprint is not fount, give us a warning
+		UE_LOG(LogTemp, Warning, TEXT("Couldn't find the RandomNumber blueprint :("));
+	}
+
+
+	// Getting  a reference to blueprint integer RandomNumber variable
+	UIntProperty* RandomNumber = FindField<UIntProperty>(this->GetClass(), TEXT("RandomNumber"));
+	if (RandomNumber != nullptr)
 	{
-		float FloatValue = FloatProp->GetPropertyValue_InContainer(this);
-		UE_LOG(LogTemp, Warning, TEXT("Float equals - %f"), FloatValue);
+		// Getting variable value and printing it
+		int32 RandomValue = RandomNumber->GetPropertyValue_InContainer(this);
+		UE_LOG(LogTemp, Warning, TEXT("Random Number is - %i"), RandomValue);
 
-		FloatProp->SetPropertyValue_InContainer(this, 48.0f);
+		// Setting a new value and printing it
+		RandomNumber->SetPropertyValue_InContainer(this, 48);
 
-		FloatValue = FloatProp->GetPropertyValue_InContainer(this);
-		UE_LOG(LogTemp, Warning, TEXT("Float equals - %f"), FloatValue);
+		RandomValue = RandomNumber->GetPropertyValue_InContainer(this);
+		UE_LOG(LogTemp, Warning, TEXT("Set Random Number is - %i"), RandomValue);
 
+	} else {
+		// if the variable is not fount, give us a warning
+		UE_LOG(LogTemp, Warning, TEXT("Couldn't find the RandomNumber blueprint variable :("));
 	}
 }
